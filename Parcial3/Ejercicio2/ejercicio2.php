06f5269ec963705e14b9be67aeb9e713cc95025d
Edna <edna.olguin.07@gmail.com>
2021-06-04 21:04:31 -0500

    Consulta PHP PDO

---
+++ Parcial3/Ejercicio2/ejercicio2.php
@@ -0,0 +1,34 @@
+
+<?php
+
+    $server='localhost';
+    $username='root';
+    $password='';
+    $database='olguinpe';
+
+    try {
+
+        $conn= new PDO("mysql:host=$server;dbname=$database", $username, $password);
+        //echo ("conectado");
+
+    } catch (PDOException $e) {
+        echo "¡Error, No se pudo conectar la base de datos!: " . $e->getMessage() . "<br/>";
+        die();
+    }
+
+    $sql="SELECT * FROM Clientes";
+    $resultado=$conn->prepare($sql);
+    $resultado->execute();
+    $data=$resultado->FetchAll(PDO::FETCH_ASSOC);
+
+
+    foreach($data as $registro){
+        echo $registro['IdCliente'].' - '.
+             $registro['Colonia'].' - '.
+             $registro['Calle'].' - '.
+             $registro['NumDomicilio'].' - '.
+             $registro['Telefono'].' - '.
+             $registro['NumMascotas'].'<br>';
+    }
+
+?>
